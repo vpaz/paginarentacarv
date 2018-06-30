afc841571eb18971894d5574889ed6f285cb2057
chaltumari <34946586+chaltumari@users.noreply.github.com>
2018-06-30 15:02:52 -0400

    Merge origin/master
    Conflicts:
    	README.md
    	nbactions.xml
    	pom.xml
    	src/main/resources/application.properties

---
+++ src/main/java/inacap/webcomponet/pagina/model/TipoVehiculo.java
@@ -0,0 +1,58 @@
+
+package inacap.webcomponet.pagina.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tipovehiculo")
+public class TipoVehiculo {
+    
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idTipoVehiculo;
+    private String nombreTipoVehiculo,detalle;
+
+    public TipoVehiculo() {
+    }
+
+    public TipoVehiculo(String nombreTipoVehiculo, String detalle) {
+        this.nombreTipoVehiculo = nombreTipoVehiculo;
+        this.detalle = detalle;
+    }
+
+    public TipoVehiculo(int idTipoVehiculo, String nombreTipoVehiculo, String detalle) {
+        this.idTipoVehiculo = idTipoVehiculo;
+        this.nombreTipoVehiculo = nombreTipoVehiculo;
+        this.detalle = detalle;
+    }
+
+    public int getIdTipoVehiculo() {
+        return idTipoVehiculo;
+    }
+
+    public void setIdTipoVehiculo(int idTipoVehiculo) {
+        this.idTipoVehiculo = idTipoVehiculo;
+    }
+
+    public String getNombreTipoVehiculo() {
+        return nombreTipoVehiculo;
+    }
+
+    public void setNombreTipoVehiculo(String nombreTipoVehiculo) {
+        this.nombreTipoVehiculo = nombreTipoVehiculo;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+    
+    
+}
---
+++ src/main/java/inacap/webcomponet/pagina/model/Traccion.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inacap.webcomponet.pagina.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="traccion")
+public class Traccion {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idTraccion;
+    private String nombreTraccion, detalle;
+
+    public Traccion() {
+    }
+
+    public Traccion(String nombreTraccion, String detalle) {
+        this.nombreTraccion = nombreTraccion;
+        this.detalle = detalle;
+    }
+
+    public Traccion(int idTraccion, String nombreTraccion, String detalle) {
+        this.idTraccion = idTraccion;
+        this.nombreTraccion = nombreTraccion;
+        this.detalle = detalle;
+    }
+
+    public int getIdTraccion() {
+        return idTraccion;
+    }
+
+    public void setIdTraccion(int idTraccion) {
+        this.idTraccion = idTraccion;
+    }
+
+    public String getNombreTraccion() {
+        return nombreTraccion;
+    }
+
+    public void setNombreTraccion(String nombreTraccion) {
+        this.nombreTraccion = nombreTraccion;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+    
+    
+    
+}
